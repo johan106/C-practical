b51fb69058c971fce1a8cbeca499654ce7241a79
johan106 <johnandsam32@gmail.com>
2025-08-04 11:59:27 +0530

    Initial commit

---
+++ practical2/a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float a,b,c,discriminant,root1,root2;
+        printf("Enter the 3 coefficient:");
+        scanf("%f %f %f",&a,&b,&c);
+        discriminant=(b*b)-(4*a*c);
+        if (discriminant>0)
+        {
+           
+        }
+        
+    return 0;
+}
